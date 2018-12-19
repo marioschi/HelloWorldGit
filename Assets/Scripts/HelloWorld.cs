702c9c3ee7555160ab8c2a59366e38fb9ea3d74e
Mario Hiebl-Rausch <mariohiebl-rausch@Marios-MacBook-Pro.local>
2018-12-19 10:39:07 +0100

    Init Project

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Console.WriteLine("Hello World");// keine Ausgabe in unity console
+        Debug.Log("Hello World");
+        print("Hello Wooorld"); //second method
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
